9940655ea943390b14598a20a25d99b991d8e5dd
leloberg <larserikloberg@gmail.com>
2016-07-12 19:58:01 +0200

    Initial

---
+++ src/oim/test/ConnectOIM.java
@@ -0,0 +1,35 @@
+package oim.test;
+
+import java.util.Hashtable;
+
+import javax.security.auth.login.LoginException;
+
+import oracle.iam.platform.OIMClient;
+
+public class ConnectOIM {
+
+	private static OIMClient oimClient;
+	
+	public static void main(String[] args) throws LoginException {
+		System.out.println("Creating client....");
+		String ctxFactory = "weblogic.jndi.WLInitialContextFactory";
+        String serverURL = "t3://oimserver:14000";
+        String username = "xelsysadm";
+        char[] password = "Admin1234".toCharArray(); //TODO Hide clear text password
+        
+    	//setting system properties
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(OIMClient.JAVA_NAMING_FACTORY_INITIAL,ctxFactory);
+        env.put(OIMClient.JAVA_NAMING_PROVIDER_URL, serverURL);
+		System.setProperty("APPSERVER_TYPE", "wls");
+		System.setProperty("java.security.auth.login.config", "conf/authwl.conf");
+		System.setProperty("weblogic.Name", "WLS_OIM1");
+
+        oimClient = new OIMClient(env);
+        System.out.println("Logging in");               
+        oimClient.login(username, password);
+        System.out.println("Log in successful");
+
+	}
+
+}
